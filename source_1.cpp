24e9b610d12f78025d1f8e599d0db8c0a7ff23cc
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-03-10 09:01:22 +0300

    Create source_1.cpp

---
+++ source_1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <fstream>
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, ".UTF8");
+	std::ifstream fin("in.txt");
+	std::string s;
+
+	if (fin.is_open()) {
+		while (!(fin >> s).eof())
+		{
+			std::cout << s << std::endl;
+		}
+	}
+	else {
+		std::cout << "Не удалось открыть файл";
+		return 1;
+	}
+
+	return 0;
+}
