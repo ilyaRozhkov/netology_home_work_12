3a77705bd20007d96c932a4d0ae9d0f3c917cb75
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-03-10 09:19:26 +0300

    Create source_2.cpp

---
+++ source_2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <fstream>
+
+int main(int argc, char** argv)
+{
+	setlocale(LC_ALL, ".UTF8");
+	std::ifstream fin("in.txt");
+	int s;
+	int size;
+	int iterator = 0;
+
+	if (fin.is_open()) {
+
+		fin >> size;
+		int* arr = new int[size];
+			for (int i = 0; i < size; i++) {
+				fin >> s;
+				arr[i] = s;
+		}
+			for (int i = size-1; i >= 0; i--) {
+				std::cout << arr[i]<< " ";
+			}
+
+	}
+	else {
+		std::cout << "Не удалось открыть файл";
+		return 1;
+	}
+
+	return 0;
+}
